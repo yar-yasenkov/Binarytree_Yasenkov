5b3f6ff61ae8ac2a7498fcc1e052e1f869b4be76
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-10 21:13:15 +0300

    Update Binarysearchtree.cpp

--- sources/Binarysearchtree.cpp
+++ sources/Binarysearchtree.cpp
@@ -6,18 +6,18 @@
 #include "tree.h"
 #include "stdlib.h"
 using namespace std;
-int main()
+/*int main()
 {
 	int a;
 	Tree intTree;           
 	intTree.input("in.txt");
-	cout << endl << "inorder_walk:" << endl;              /* обходим */
-	intTree.inorder_walk(intTree.get_root());       /* вот для этого понадобился метод get_root() :-) */
+	cout << endl << "inorder_walk:" << endl;            
+	intTree.inorder_walk(intTree.get_root());      
 	cout << "number:";
 	cin >> a;
 	intTree.insert_node(a);
-	cout << endl << "inorder_walk:" << endl;              /* обходим */
+	cout << endl << "inorder_walk:" << endl;              
 	intTree.inorder_walk(intTree.get_root());
 	system("pause");
 
-}
+}*/
