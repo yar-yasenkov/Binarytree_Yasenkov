5fc3328a5310539d47369e30acf16738456e9a45
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-11 21:31:19 +0300

    Update Node.hpp

--- include/Node.hpp
+++ include/Node.hpp
@@ -1,28 +1,29 @@
 #ifndef A_H_INCLUDED 
 #define A_H_INCLUDED
- class TreeNode
+template <typename T> class TreeNode
 {
-	friend class Tree;
+	friend class Tree<T>;
 public:
 	TreeNode();
-	TreeNode(const int &);
-	int get_data();
+	TreeNode(const T &);
+	T get_data();
 protected:
 	TreeNode* left;           /* óêàçàòåëü íà ëåâîãî ðåáåíêà */
 	TreeNode* right;          /* óêàçàòåëü íà ïðàâîãî ðåáåíêà */
 	TreeNode* parent;         /* óêàçàòåëü íà ðîäèòåëÿ */
-	int data;            /* êëþ÷ */
+	T data;            /* êëþ÷ */
 };
 
  TreeNode::TreeNode() :data(0), left(0), right(0)
  {}
-TreeNode::TreeNode(const int &a)
+TreeNode::TreeNode(const T &a)
 {
 	data = a;
 	left = right = 0;
 }
 
-int TreeNode::get_data()
+template <typename T>
+T TreeNode <T>::get_data()
 {
 	return data;
 }
