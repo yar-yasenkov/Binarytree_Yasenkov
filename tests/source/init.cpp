2778534251744d7dde7a0b2378c588c6b4d0c323
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-11 20:20:25 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -9,21 +9,20 @@ SCENARIO("Tree init", "[init]") {
          mark=inttree.input("in.txt");
 	 REQUIRE(mark);
 }
+
+SCENARIO("Insert node","[insert]") {
+	Tree inttree;
+	bool mark=false;
+	mark=inttree.insert_node(125);
+	REQUIRE(mark);
+}
+
+
 /*
 SCENARIO("Matrix >>", "[fill]") {
 	Matrix A = Matrix(2, 2);
 	REQUIRE( A.fill("A2x2.txt") );
 }
 
-SCENARIO("Matrix +", "[addition]") {
-	Matrix A = Matrix(2, 2);
-	A.fill("A2x2.txt");
-	Matrix B = Matrix(2, 2);
-	B.fill("B2x2.txt");
-	Matrix expected = Matrix(2, 2);
-	expected.fill("A+B2x2.txt");
 
-	Matrix result = A + B;
-	REQUIRE(result == expected);
-}
 */
