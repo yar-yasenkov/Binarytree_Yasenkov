a5cbc0cacfe0821a0952e249e2e4d1d530160f87
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-18 12:38:18 +0300

    Update tree.hpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -134,7 +134,7 @@ TreeNode<T>* Tree<T>::find_node(TreeNode<T>* n,
 	if ((n==0) && (marker==0))
 	{
 		marker++;
-		throw Empty_tree();
+		//throw Empty_tree();
 	}
 	if (n == 0 || val == n->get_data())
 		return n;
