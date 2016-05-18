b9c3aadf6d14c9c84196d2009d37c39d75dc864b
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-18 10:19:19 +0300

    Update tree.hpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -63,7 +63,7 @@ private:
 	TreeNode<T> *root;                                  /* ñîáñòâåííî, ñàì êîðåíü */
 };
 
-        Already_exist::Already_exist() : Exception("ERROR: element already exists!") {}
+        Tree::Exception::Already_exist::Already_exist() : Exception("ERROR: element already exists!") {}
         File_Not_Open::File_Not_Open() : Exception("ERROR: file not open!") {}
         Empty_tree::Empty_tree() : Exception("ERROR: tree is empty!") {}
         Element_not_found::Element_not_found() : Exception("ERROR: this element does not exist!") {}
