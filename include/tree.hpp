4e8c59364888cd362e6093c06c1c774beaa955a3
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-19 20:34:29 +0300

    Update tree.hpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -62,7 +62,7 @@ public:
         
         Tree();                                                    /* êîíñòðóêòîð */
 	TreeNode<T>* find_node(TreeNode<T>*, const T &) const;
-	bool search(TreeNode<T>*,  T const &);
+	bool search(TreeNode<T>*,const  T  &);
 	bool insert_node(const T &);                         /* âñòàâëÿåò óçåë */
 	bool input(const string &);
 	bool output(TreeNode<T>*) const;
@@ -199,7 +199,7 @@ bool Tree<T>::output(TreeNode<T>* n) const
 }
 
 template <typename T>
-bool search(TreeNode<T>* n,const T & value)
+bool Tree<T>::search(TreeNode<T>* n,const T & value)
 {
 	if ( value == n->get_data())
 	{
