f8bacf06ca15c1ccde3e563055139b53fea4847f
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-05-19 20:43:10 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -100,6 +100,7 @@ SCENARIO("Delete root","[delete r ]") {
 	 inttree.input("in.txt");
 	 //удаление корня
 	 inttree.delete_node(inttree.get_root());
+	 REQUIRE(!inttree.search(inttree.get_root(),5));
 	 REQUIRE(inttree.search(inttree.get_root(),4));
 	 REQUIRE(inttree.search(inttree.get_root(),2));
 	 REQUIRE(inttree.search(inttree.get_root(),3));
@@ -112,6 +113,7 @@ SCENARIO("Delete node","[delete n]") {
 	 TreeNode<int>* node;
 	 node=inttree.find_node(inttree.get_root(),4);
 	 inttree.delete_node(node);
+	 REQUIRE(!inttree.search(inttree.get_root(),4));
 	 REQUIRE(inttree.search(inttree.get_root(),5));
 	 REQUIRE(inttree.search(inttree.get_root(),2));
 	 REQUIRE(inttree.search(inttree.get_root(),3));
